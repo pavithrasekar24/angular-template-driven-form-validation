966680aad5dd48cf46c5b6ee467eff94fcd8a127
pavithrasekar24 <spavithracse8@gmail.com>
2022-06-30 18:40:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-10-template-driven-form-validation-diqyra
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-10-template-driven-form-validation-diqyra)
---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
+export class AppComponent {
+  @ViewChild(NgForm) form;
+  model: any = {};
+
+  ngAfterViewInit() {
+    console.log('FORM', this.form);
+  }
+
+  onSubmit() {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model, null, 4));
+  }
+}
